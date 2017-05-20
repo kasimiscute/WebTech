98da5f6cd2ee4bcd21d08b185faf7a3814ac8bcf
rui meng <rmeng3@sheffield.ac.uk>
2017-05-20 09:44:23 +0100

    Part2

--- solution/server/frequency.js
+++ solution/server/frequency.js
@@ -1,9 +1,15 @@
  var express = require("express");
  var path = require('path');
  var fs = require('fs');
- var app = express();
+ //var app = express();
  var bodyParser = require('body-parser');
  var utf = require('utf8');
+ var url = require('url');
+ var protocol = require('http');
+ var static = require('node-static');
+ var util = require('util');
+ var file = new (static.Server)();
+ var portNo = 3000;
 
  var Twit = require('twit');
  var client = new Twit({
@@ -13,14 +19,24 @@
   access_token_secret: 'L48tLAUaJ8FFikjgJHkQafQ2iHttXeOwuKEarnfT3vCsF'
 });
 
+var datearray = new Array([]);
 
 //  search twitter for all tweets containing the word 'banana'
-// since Nov. 11, 2011
-app.use(bodyParser());
 
-app.get('/', function(req, res) {
-	res.sendFile('chart.html', {root: path.join(__dirname)});
+
+var TEST_TABLE = 'datatest';
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+	user     : 'root',
+	password : 'Mr123456',
+	database : 'test',
+  dateStrings: 'date',
 });
+connection.connect();
+
 
 function GetCurTime(){
   var d = new Date();
@@ -36,10 +52,12 @@ function add_zero(temp){
   else return temp;
 }
 var n=1;
+
 Date.prototype.minusDays = Date.prototype.minusDays || function(n) {
   this.setDate(this.getDate() - n);
   return this;
 };
+
 function minusDate(date,days){
   var d = new Date(date);
   d.setDate(d.getDate() - days);
@@ -49,47 +67,92 @@ function minusDate(date,days){
 }
 
 
-app.post('/frequency',function(req,res){
+var app = protocol.createServer(function (req, res) {
+	var pathname = url.parse(req.url).pathname;
+	var body = '';
 
-res.writeHead(200, {"Content-Type": 'text/html', 'charset': 'utf-8'});
-console.log(req.body.query);
-var queryInput = JSON.stringify(req.body.query);
+	if ((req.method == 'POST') && (pathname == '/index.html')) {
+		var dataFin = {ok: 'ok'};
+		req.on('data', function (data) {
+			body += data;
+		});
 
-var currentDate = GetCurTime();
-//for(var i=1; i<7;i++){
-var pastDate = minusDate(currentDate,1);
-var searchquery = queryInput +'since:'+ pastDate +' until:' + currentDate ;
+		req.on('end', function () {
+			body= JSON.parse(body);
 
-  console.log('query:'+ searchquery);
-  //a = b;
-//}
-var count=0;
-var i=0;
-{
-  runQuery(queryInput,searchquery,currentDate,pastDate,count,i);
-}
-res.end();
+
+      var currentDate = GetCurTime();
+      var endDate = minusDate(currentDate,7);
+      console.log(currentDate);
+
+      var datePack = {"date":[]};
+      var frequencyPack = {"frequency" : []};
+      var frquency = 0;
+      var i = 0;
+
+      runQuery( currentDate,endDate ,frequencyPack, frquency, res);
 });
+	}
+	else {
+		file.serve(req, res, function (err, result) {
+			if (err !== null) {
+                        //console.error('url: '+req.url);
+                        //console.error('err: '+err.message);
+                        console.error('Error serving %s - %s', req.url, err.message);
+                        if (err.status === 404 || err.status === 500) {
+                        	file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+                        } else {
+                        	res.writeHead(err.status, err.headers);
+                        	res.end();
+                        }
+                    } else {
+                    	res.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin': '*'});
 
-function runQuery(queryInput,searchquery,currentDate,pastDate,count,i){
-  i++;
-  client.get('search/tweets', { q: searchquery, count: 100 },
-              function(err, data, response) {
-                if(err) return console.error(err);
-                  for (var indx in data.statuses) {
-                      var tweet= data.statuses[indx];
-                      count++;
-                  }
-                    console.log('p:'+pastDate+'c'+currentDate+'count:'+ count);
-                  currentDate = pastDate;
-                  pastDate = minusDate(currentDate,1);
-                  searchquery = queryInput +'since:'+ pastDate +' until:' + currentDate ;
-                  count = 0;
-                  if(i<8)
-                  {
-                    runQuery(queryInput,searchquery,currentDate,pastDate,count,i);
-                  }
-  });
+                    }
+                });
+	}
+}).listen(3000);
+
+
+function runQuery(currentDate,endDate,frequencyPack, frquency, res){
+  var userGetSql =  'SELECT date from datatest where date = ?';
+  var userGetSql_Params = [currentDate];
+  var pastDate = minusDate(currentDate,1);
+    connection.query(userGetSql,userGetSql_Params,function selectCb(error,results,fields){
+
+    if(error) {console.log('[SELECT ERROR] - ',error.message);return;}
+      if(results){
+    if(currentDate >= endDate){
+          frequency=0;
+          for (var i=0; i<results.length; i++) {
+              var firstResult = results[i];
+              frequency++;
+          }
+          var frequencyObj = {
+            "number": frequency,
+            "currentDate": currentDate,
+            "pastdate": pastDate,
+          };
+
+          frequencyPack.frequency.push(frequencyObj);
+
+            currentDate = pastDate;
+            pastDate = minusDate(currentDate,1);
+            runQuery(currentDate,endDate,frequencyPack,frquency, res);
+          }
+    else{
+            console.log("results..................................");
+            console.log(frequencyPack);
+            res.writeHead(200, { "Content-Type": "application/json"});
+            res.end(JSON.stringify(frequencyPack));
+            connection.end();
+
+          }
+
+      }
+
+    });
+
 }
 
 app.listen(3000, function() {
